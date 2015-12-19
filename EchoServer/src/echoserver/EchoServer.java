19769c4c4b09c2dd96bdd7d8125e6fa18130ffa1
Ricardo Ortiz <ricardo.mpcs@gmail.com>
2015-12-18 19:18:39 -0500

    cambio al puerto pero solo establece una conexion

--- EchoServer/src/echoserver/EchoServer.java
+++ EchoServer/src/echoserver/EchoServer.java
@@ -9,14 +9,18 @@ package echoserver;
  * @author gato
  */
 import java.net.*;
+import java.util.ArrayList;
 import java.io.*;
 
 public class EchoServer {
 
     public static void main(String[] args) throws IOException {
-
-/*
-        int portNumber = 9999;
+    	ArrayList<Integer> clientes = new ArrayList<Integer>();
+    	EchoClient cliente= new EchoClient();
+    	//Socket e=cliente.portNumber;
+    	clientes.add(Integer.valueOf(cliente.portNumber));
+    	 	    
+       /*int portNumber = 9999;
 
         try (
                ServerSocket serverSocket =
@@ -40,16 +44,15 @@ public class EchoServer {
 
         NuevoSocket iniciar = new NuevoSocket();
         iniciar.run();
-        CorrerSocket t1 = new CorrerSocket(iniciar.getServer(), "thread 1");
-        CorrerSocket t2 = new CorrerSocket(iniciar.getServer(), "thread 2");
-        t1.start();
-        t2.start();
-
+        for (int i=0;i<clientes.size();i++){
+        CorrerSocket t = new CorrerSocket(iniciar.getServer(), "thread "+i);
+        t.start();
         try {
-            t1.join();
-            t2.join();
+            t.join();
+            
         } catch (Exception e) {
         
+        }
         }
     }
 }
