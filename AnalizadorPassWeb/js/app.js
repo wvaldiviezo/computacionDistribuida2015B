2c237363cb67ead599c97c4ed7f01e458155b785
Ricardo Ortiz <ricardo.mpcs@gmail.com>
2016-02-12 17:54:14 -0500

    Cambio en app.js combio por incompatibilidad de caracteres. problemas ocacionados por el editor utilizado

--- AnalizadorPassWeb/js/app.js
+++ AnalizadorPassWeb/js/app.js
@@ -3,8 +3,8 @@ var app = angular.module('app', []);
 app.controller('main', ['$scope', '$http', function($scope, $http) {
   $scope.comprobar = function() {
     console.log($scope.pwd);
-    var expreg = /(^(?=.*[a-z@])(?=.*[@*/_.¡?'¿ñÑ!"#$%&])(?=.*[A-Z])(?=.*\d){6,20}.+$)/;
-	
+    var expreg = /(^(?=.*[a-z@])(?=.*[@*/_.!?'Â¿Ã‘Ã±Â¡"#$%&+-])(?=.*[A-Z])(?=.*\d){10,}.+$)/;
+
 //var expreg = /^[A-Z]{1,2}\s\d{4}\s([B-D]|[F-H]|[J-N]|[P-T]|[V-Z]){3}$/;
     if (expreg.test($scope.pwd))
       alert("Password segura");
